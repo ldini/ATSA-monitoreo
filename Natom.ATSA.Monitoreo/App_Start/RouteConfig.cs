166beac129ff253a076d3de9404b428a2440d11e
Leandro <leandro.david.dini@gmail.com>
2023-05-29 15:44:30 -0300

    fecha al documento descargado

--- Natom.ATSA.Monitoreo/App_Start/RouteConfig.cs
+++ Natom.ATSA.Monitoreo/App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace Natom.ATSA.Monitoreo
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "monitoreotest/{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
